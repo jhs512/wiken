3361ee40ab2eb61ab4e8867eca40af8de908f3e3
jhs512 <jangka512@gmail.com>
2022-05-29 11:20:36 +0900

    하하

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -5,7 +5,9 @@ const getFormData = object => Object.keys(object).reduce((formData, key) => {
 
 function strToHtmlHash(str)
 {
-    return str.replace(/ /gi, "-");
+  return str
+    .replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
+    .replace(/ /gi, "-");
 }
 
 let needToGoHashLater = false;
